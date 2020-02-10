6725d8910691daea830d95d302a495c51642a3cb
cyct123 <cyct123@gmail.com>
2020-02-10 19:54:06 +0800

    Style(Percolation): variable name use camelCase; fix method isFull bug

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -7,9 +7,10 @@ import edu.princeton.cs.algs4.StdRandom;
 public class Percolation {
 
     private final int size;
-    private WeightedQuickUnionUF sites_uf;
-    private boolean[][] sites;
-    private int open_sites;
+    private final WeightedQuickUnionUF percolateSitesUF;
+    private final WeightedQuickUnionUF fullSitesUF;
+    private final boolean[][] sites;
+    private int openSites;
     private final int top;
     private final int bottom;
 
@@ -18,12 +19,13 @@ public class Percolation {
         if (n <= 0) throw new IllegalArgumentException("n must be bigger than 1");
         size = n;
         sites = new boolean[n][n];
-        sites_uf = new WeightedQuickUnionUF(size * size + 2);
+        percolateSitesUF = new WeightedQuickUnionUF(size * size + 2);
+        fullSitesUF = new WeightedQuickUnionUF(size * size + 1);
         top = 0;
         bottom = size * size + 1;
     }
 
-    private int point_index(int row, int col) {
+    private int pointIndex(int row, int col) {
         validate(row, col);
         return (row - 1) * size + col;
     }
@@ -35,32 +37,35 @@ public class Percolation {
             throw new IllegalArgumentException("row must be between 1 and " + size);
     }
 
-    private void connect(int cur_idx, int neighbor_row, int neighbor_col) {
+    private void connect(int curIdx, int neighborRow, int neighborCol) {
         try {
-            validate(neighbor_row, neighbor_col);
+            validate(neighborRow, neighborCol);
         } catch (IllegalArgumentException e) {
             return;
         }
-        if (!isOpen(neighbor_row, neighbor_col)) return;
-        int neighbor_idx = point_index(neighbor_row, neighbor_col);
-        sites_uf.union(cur_idx, neighbor_idx);
+        if (!isOpen(neighborRow, neighborCol)) return;
+        int neighborIdx = pointIndex(neighborRow, neighborCol);
+        percolateSitesUF.union(curIdx, neighborIdx);
+        fullSitesUF.union(curIdx, neighborIdx);
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         validate(row, col);
         if (isOpen(row, col)) return;
-        int cur_idx = point_index(row, col);
-        if (row == 1)
-            sites_uf.union(top, cur_idx);
-        else if (row == size)
-            sites_uf.union(bottom, cur_idx);
-        connect(cur_idx, row - 1, col);
-        connect(cur_idx, row + 1, col);
-        connect(cur_idx, row, col - 1);
-        connect(cur_idx, row, col + 1);
+        int curIdx = pointIndex(row, col);
+        if (row == 1) {
+            percolateSitesUF.union(top, curIdx);
+            fullSitesUF.union(top, curIdx);
+        }
+        if (row == size)
+            percolateSitesUF.union(bottom, curIdx);
+        connect(curIdx, row - 1, col);
+        connect(curIdx, row + 1, col);
+        connect(curIdx, row, col - 1);
+        connect(curIdx, row, col + 1);
         sites[row - 1][col - 1] = true;
-        open_sites++;
+        openSites++;
     }
 
     // is the site (row, col) open?
@@ -72,18 +77,18 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         validate(row, col);
-        int idx = point_index(row, col);
-        return sites_uf.connected(top, idx);
+        int idx = pointIndex(row, col);
+        return fullSitesUF.connected(top, idx);
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return open_sites;
+        return openSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return sites_uf.connected(top, bottom);
+        return percolateSitesUF.connected(top, bottom);
     }
 
     // test client (optional)
@@ -97,12 +102,23 @@ public class Percolation {
         }
         StdOut.println(p.numberOfOpenSites());
         StdOut.println(p.percolates());
-        int rand_test = StdRandom.uniform(size) + 1;
-        for (int i = 0; i < rand_test; i++) {
-            int rand_row = StdRandom.uniform(size) + 1;
-            int rand_col = StdRandom.uniform(size) + 1;
-            StdOut.println("" + rand_row + ", " + rand_col + " isOpen: " + p.isOpen(rand_row, rand_col));
-            StdOut.println("" + rand_row + ", " + rand_col + " isFull: " + p.isFull(rand_row, rand_col));
+        int randTest = StdRandom.uniform(size) + 1;
+        for (int i = 0; i < randTest; i++) {
+            int randRow = StdRandom.uniform(size) + 1;
+            int randCol = StdRandom.uniform(size) + 1;
+            StdOut.println(
+                    "" + randRow + ", " + randCol + " isOpen: " + p.isOpen(randRow, randCol));
+            StdOut.println(
+                    "" + randRow + ", " + randCol + " isFull: " + p.isFull(randRow, randCol));
         }
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                StdOut.println("" + i + ", " + j + " isOpen: " + p.isOpen(i, j));
+                StdOut.println("" + i + ", " + j + " isFull: " + p.isFull(i, j));
+            }
+        }
+        int i = 1;
+        int j = 18;
+        StdOut.println("" + i + ", " + j + " isFull: " + p.isFull(i, j));
     }
 }
