0b980a1692b495cc2c509e878c9055f376d0ac86
cyct123 <cyct123@gmail.com>
2020-02-10 16:14:12 +0800

    Implemented PercolationStats

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,21 +1,55 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdOut;
+
+
 public class PercolationStats {
 
+    private final int T;
+    private double[] frequency;
+
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials)
+    public PercolationStats(int n, int trials) {
+        T = trials;
+        frequency = new double[trials];
+        for (int i = 0; i < T; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                int rand_row = StdRandom.uniform(n) + 1;
+                int rand_col = StdRandom.uniform(n) + 1;
+                p.open(rand_row, rand_col);
+            }
+            frequency[i] = (double) p.numberOfOpenSites() / (n * n);
+        }
+    }
 
     // sample mean of percolation threshold
-    public double mean()
+    public double mean() {
+        return StdStats.mean(frequency);
+    }
 
     // sample standard deviation of percolation threshold
-    public double stddev()
+    public double stddev() {
+        return StdStats.stddev(frequency);
+    }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo()
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi()
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 
     // test client (see below)
-    public static void main(String[] args)
-
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, trials);
+        StdOut.printf("%-24s= %.16f\n", "mean", ps.mean());
+        StdOut.printf("%-24s= %.16f\n", "stddev", ps.stddev());
+        StdOut.printf("%-24s= [%.16f, %.16f]\n", "95% confidence interval", ps.confidenceLo(), ps.confidenceHi());
+    }
 }
