82587a881994b9d2769f4d8135f928ff19932815
cyct123 <cyct123@gmail.com>
2020-02-10 20:27:32 +0800

    Style(PercolationStats):Declare static variables before instance variables, constructors, and methods.

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -5,9 +5,9 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
 
+    private static final double CONFIDENCE_95 = 1.96;
     private final int trials;
     private final double[] frequency;
-    private static final double CONFIDENCE_95 = 1.96;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
