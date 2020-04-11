730e8800a74b1482ba47efa6a0df195b431d2c35
cyct123 <cyct123@gmail.com>
2020-04-11 11:38:35 +0800

    feat: put calculations in constructor

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -8,6 +8,10 @@ public class PercolationStats {
     private static final double CONFIDENCE_95 = 1.96;
     private final int trials;
     private final double[] frequency;
+    private final double meanVal;
+    private final double stdDevVal;
+    private final double confidenceLoVal;
+    private final double confidenceHiVal;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
@@ -24,26 +28,30 @@ public class PercolationStats {
             }
             frequency[i] = (double) p.numberOfOpenSites() / (n * n);
         }
+        meanVal = StdStats.mean(frequency);
+        stdDevVal = StdStats.stddev(frequency);
+        confidenceLoVal = meanVal - CONFIDENCE_95 * stdDevVal / Math.sqrt(trials);
+        confidenceHiVal = meanVal + CONFIDENCE_95 * stdDevVal / Math.sqrt(trials);
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return StdStats.mean(frequency);
+        return meanVal;
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return StdStats.stddev(frequency);
+        return stdDevVal;
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean() - CONFIDENCE_95 * stddev() / Math.sqrt(trials);
+        return confidenceLoVal;
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + CONFIDENCE_95 * stddev() / Math.sqrt(trials);
+        return confidenceHiVal;
     }
 
     // test client (see below)
