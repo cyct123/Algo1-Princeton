a51d18ddb13046f6fee82c50375dcde52cf5a7a6
chengziwei <chengziwei@mininglamp.com>
2020-05-27 20:15:32 +0800

    feat: finish Mystery.java

---
+++ chapter1/Mystery.java
@@ -0,0 +1,18 @@
+public class Mystery {
+
+    public static String mystery(String s)
+    {
+        int N = s.length();
+        if (N <= 1) return s;
+        String a = s.substring(0, N/2);
+        String b = s.substring(N/2, N);
+        return mystery(b) + mystery(a);
+    }
+
+    public static void main(String[] args)
+    {
+        String s = "Hello World";
+        String newS = mystery(s);
+        System.out.println(newS);
+    }
+}
